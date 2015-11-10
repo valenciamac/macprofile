45fc70104a9ec8a1439859f3f307c15f9de13e8b
mark anthony valencia <mac01218@yahoo.com>
2015-11-10 10:12:09 +0800

    create database

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
 <head>
 	<title>Welcome to my Portfolio</title>
@@ -10,4 +10,29 @@
 		<h1>Site Under Construction</h1>
 	</div>
 </body>
-</html>
+</html> -->
+
+<?php
+
+	$link = mysql_connect('u569767831_nativ','u569767831_markv', '848501307');
+	if (!$link){
+		die('could not connect to database' . mysql_error());
+	}
+
+	$db_selected = my_sql_select_db('myprofile', $link);
+
+	if (!$db_selected)
+	{
+		$sql = 'CREATE DATABASE myprofile';
+
+		if (mysql_query($sql, $link))
+		{
+			echo "Database my_db created successfully\n";
+		}
+		else
+		{
+			echo 'Error creating database' . mysql_error() . "\n";
+		}
+	}
+	mysql_close($link);
+?>
